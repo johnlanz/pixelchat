3e8c8dbdbae13db277a069902b266cc11e76c48a
John Lanz <johnlanz@live.com>
2020-07-04 01:37:48 +0800

    add handshake

--- app/WebsocketServer.php
+++ app/WebsocketServer.php
@@ -44,6 +44,43 @@ class WebsocketServer
             $this->onWorkerStart($this->ws);
         });
 
+        $this->ws->on('handshake', function (\swoole_http_request $request, \swoole_http_response $response) {
+            $secWebSocketKey = $request->header['sec-websocket-key'];
+            $patten = '#^[+/0-9A-Za-z]{21}[AQgw]==$#';
+            
+            if (0 === preg_match($patten, $secWebSocketKey) || 16 !== strlen(base64_decode($secWebSocketKey))) {
+                $response->end();
+                return false;
+            }
+            
+            echo $request->header['sec-websocket-key'];
+            
+            $key = base64_encode(sha1($request->header['sec-websocket-key'] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', true));
+        
+            $headers = [
+                'Upgrade' => 'websocket',
+                'Connection' => 'Upgrade',
+                'Sec-WebSocket-Accept' => $key,
+                'Sec-WebSocket-Version' => '13',
+            ];
+        
+            // WebSocket connection to 'ws://127.0.0.1:9502/'
+            // failed: Error during WebSocket handshake:
+            // Response must not include 'Sec-WebSocket-Protocol' header if not present in request: websocket
+            if (isset($request->header['sec-websocket-protocol'])) {
+                $headers['Sec-WebSocket-Protocol'] = $request->header['sec-websocket-protocol'];
+            }
+        
+            foreach ($headers as $key => $val) {
+                $response->header($key, $val);
+            }
+        
+            $response->status(101);
+            $response->end();
+            echo "connected!" . PHP_EOL;
+            return true;
+        });
+
         $this->ws->start();
     }
 
